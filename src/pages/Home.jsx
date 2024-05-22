1203c42b1bf0f8acdacabe9af44928a04c84cfd1
franco22bellido <franco.bellido22@gmail.com>
2024-05-22 04:43:29 -0300

    deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useNavigate, useParams } from "react-router-dom"
 import { useUrls } from "../context/UrlContext"
-import { UseAuth } from "../context/authContext"
+import { UseAuth } from "../context/AuthContext"
 import { useEffect } from "react"
 import SectionContainer from "../components/SectionContainer"
 import FormUrl from "../components/FormUrl"
